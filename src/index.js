b54fe8f84565da36eb524eadd8c46eef7cbf3c2e
AtulKaushik-hub <atulkau99@gmail.com>
2019-12-07 23:47:59 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 
 
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
